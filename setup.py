11a3f1f8aea4c6be2a6a9cb837baea4b6d5ca967
miketrumpis <mtrumpis@gmail.com>
2019-08-06 14:46:22 -0700

    get rid of cython parts

--- setup.py
+++ setup.py
@@ -48,18 +48,6 @@ opts = dict(name=NAME,
             python_requires=PYTHON_REQUIRES,
             )
 
-try:
-    from distutils.extension import Extension
-    from Cython.Distutils import build_ext as build_pyx_ext
-    from numpy import get_include
-    # add Cython extensions to the setup options
-    exts = [Extension('nitime._utils', ['nitime/_utils.pyx'],
-                      include_dirs=[get_include()])]
-    opts['cmdclass'] = dict(build_ext=build_pyx_ext)
-    opts['ext_modules'] = exts
-except ImportError:
-    # no loop for you!
-    pass
 
 # Now call the actual setup function
 if __name__ == '__main__':
